b4cef2762a72ee69df1e6c67f0f3e300c3f602cf
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-03 10:47:45 -0300

    Converter repositório para Spring-data

--- src/main/java/br/com/asty/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/asty/mvc/mudi/repository/PedidoRepository.java
@@ -1,27 +1,12 @@
 package br.com.asty.mvc.mudi.repository;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.asty.mvc.mudi.model.Pedido;
 
 @Repository
-public class PedidoRepository {
-    
+public interface PedidoRepository extends JpaRepository<Pedido, Long>{
 
-    @PersistenceContext
-    private EntityManager em;
-
-
-    public List<Pedido> recuperaTodos(){
-        Query query = em.createQuery("select p from Pedido p", Pedido.class);
-        List<Pedido> pedidos = query.getResultList();
-       return pedidos;
-    }
 
 }
