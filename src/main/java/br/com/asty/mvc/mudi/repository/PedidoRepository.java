f7b120d6e46a31ff5466a530f22c81f2addc5a5b
Paulo Roberto Campos de Vasconcelos <paulorcv@gmail.com>
2021-10-03 21:55:40 -0300

    Adicionando filtro por status

--- src/main/java/br/com/asty/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/br/com/asty/mvc/mudi/repository/PedidoRepository.java
@@ -1,12 +1,16 @@
 package br.com.asty.mvc.mudi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import br.com.asty.mvc.mudi.model.Pedido;
+import br.com.asty.mvc.mudi.model.StatusPedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido, Long>{
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    List<Pedido> findByStatus(StatusPedido aprovado);
 
 }
